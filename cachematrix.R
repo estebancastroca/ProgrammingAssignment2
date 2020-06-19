55c64743b3673f449ed5f2de96fb5a31459bcb46
estebancastroca <66756497+estebancastroca@users.noreply.github.com>
2020-06-19 16:27:06 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
 }
+
+tmatrix <- makeCacheMatrix(matrix(1:16, nrow = 4, ncol = 4))
+
+tmatrix$get()
+
+tmatrix$getInverse()
+
+cacheSolve(tmatrix)
+
+tmatrix <- makeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2))
+tmatrix$get()
+
+my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+my_matrix$get()
+
+pmatrix$getInverse()
+cacheSolve(tmatrix)
+
+cacheSolve(tmatrix)
+
+tmatrix$getInverse()
+
+#######
+tmatrix <- makeCacheMatrix(matrix(1:16, nrow = 2, ncol = 2))
+
+tmatrix$get()
+
+tmatrix$getInverse()
+
+cacheSolve(tmatrix)
+
+tmatrix <- makeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2))
+tmatrix$get()
+
+my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+my_matrix$get()
+
+pmatrix$getInverse()
+cacheSolve(tmatrix)
+
+cacheSolve(tmatrix)
+
+tmatrix$getInverse()
+
